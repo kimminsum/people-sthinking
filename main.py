f88165221af95d69c7b843675cc91036e6152342
kimminsum <tommy1005a@gmail.com>
2023-03-01 18:12:53 +0900

    1

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import nltk
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from collections import Counter
+from konlpy.tag import Okt
+from PIL import Image
+import numpy as np
 
 
-class Scrapping(object):
+class Scrapping:
     def __init__(self, url):
         self.url: str = url # url
         print(self.scap())
+        self.wordclouding(self.scap())
 
     """Scrapping Data -> String"""
     def scap(self) -> list[str]:
@@ -16,10 +22,12 @@ class Scrapping(object):
         # Parsing data, HTML5 -> String
         tag = soup.body
         text_list: list = []
+        extracted_text_list: list = []
         # Remove special characters and Deduplication
-        text_list = self.noun_extraction(list(set([self.clean_text(string) for string in tag.strings])))
-
-        return text_list
+        text_list = (list(set([self.clean_text(string) for string in tag.strings])))
+        for text in text_list:
+            extracted_text_list.extend(self.noun_extraction(text))
+        return extracted_text_list
 
     """Remove special characters"""
     def clean_text(self, inputString) -> str:
@@ -31,11 +39,23 @@ class Scrapping(object):
     """Extract noun"""
     def noun_extraction(self, inputString):
         # function to test if something is a noun
-        is_noun = lambda pos: pos[:2] == "NN"
-        tokenized = nltk.word_tokenize(inputString)
-        nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
+        okt = Okt()
+        nouns = okt.nouns(inputString)
+        words = [n for n in nouns if len(n) > 1]
 
-        return nouns
+        return words
+
+    """Make Word Clouding"""
+    def wordclouding(self, words):
+        img = Image.open('background.jpg')
+        img_array = np.array(img)
+        c = Counter(words) # 위에서 얻은 words를 처리하여 단어별 빈도수 형태의 딕셔너리 데이터를 구함
+        wc = WordCloud(font_path='malgun', width=400, height=400, scale=2.0, max_font_size=250, mask=img_array)
+        gen = wc.generate_from_frequencies(c)
+        plt.figure()
+        plt.imshow(gen)
+        plt.show()
+        wc.to_file("cloudword.png")
 
 
 if __name__=="__main__":
