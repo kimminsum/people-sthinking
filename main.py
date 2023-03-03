6f30d0284d5a5ab0fc88be4dd61b67cd5d252136
kimminsum <tommy1005a@gmail.com>
2023-03-03 11:51:20 +0900

    descrition complete

--- main.py
+++ main.py
@@ -38,39 +38,46 @@ class CreateWordCloud:
 
         @app.route("/", methods=["POST","GET"])
         def upload_file():
+            # Get background image from website
             if request.method == "POST":
                 f = request.files["file"]
+                # Change file name as background.png
                 f.filename = "background.png"
+                # Save background image in "./DB/background.png"
                 f.save(f"{self.database_path}/{secure_filename(f.filename)}")
-            return render_template("index.html")
+            return render_template("index.html") # rendering index.html
 
         app.run(debug=True)
 
-    """Scrapping Data -> String"""
+    """Scrapping data by list[str] format"""
     def scap(self) -> list[str]:
         page = requests.get(self.url)
+        # Get html format text
         soup = BeautifulSoup(page.content, "html.parser")
-        # Parsing data, HTML5 -> String
+        # Extract body tag
         tag = soup.body
         text_list: list = []
         extracted_text_list: list = []
         # Remove special characters and Deduplication
         text_list = (list(set([self.clean_text(string) for string in tag.strings])))
         for text in text_list:
+            # Extract noun in text
             extracted_text_list.extend(self.noun_extraction(text))
 
         return extracted_text_list
 
     """Remove special characters"""
     def clean_text(self, inputString) -> str:
+        # Remove special characters in inputString
         outputString = re.sub('[-=+,#/\?:»^.@*\"※~ㆍ!』‘|\(\©>)\[\]`\'…》\”\“\’·]', ' ', inputString)
+        # Remove space " "
         outputString = " ".join(outputString.split())
 
-        return outputString
+        return outputString # Summit converted inputString
 
     """Extract noun"""
     def noun_extraction(self, inputString):
-        # function to test if something is a noun
+        # Function to test if something is a noun
         okt = Okt()
         nouns = okt.nouns(inputString)
         words = [n for n in nouns if len(n) > 1]
@@ -80,13 +87,16 @@ class CreateWordCloud:
     """Make Word Clouding"""
     def wordclouding(self, words):
         img = Image.open(f"{self.database_path}/background.jpg")
+        # Change image to numpy array format
         img_array = np.array(img)
-        c = Counter(words) # words to dictortionary
+        c = Counter(words) # Words to dictortionary
+        # Generate word cloud
         wc = WordCloud(font_path='malgun', width=400, height=400, scale=2.0, max_font_size=250, mask=img_array)
         gen = wc.generate_from_frequencies(c)
         plt.figure()
         plt.imshow(gen)
         plt.show()
+        # Save generated image to ./DB/cloudword.png
         wc.to_file(f"{self.database_path}/cloudword.png")
 
 
